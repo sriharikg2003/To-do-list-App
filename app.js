b6b8c6eb64b004f22d1d98d26a3f990dada7e322
sriharikg2003 <sriharikg2003@gmail.com>
2023-05-04 19:03:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,198 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const date = require(__dirname + "/date.js");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const mongoose = require("mongoose");
+
+// To do list Database
+// todolist database connection
+mongoose.connect('mongodb://127.0.0.1:27017/todolist');
+
+
+
+// ****************************************************************
+
+// todolist schema
+const todolist = mongoose.Schema({
+  name: String,
+
+})
+
+// todolist model
+const Item = mongoose.model('Item', todolist, 'Items');
+
+// todolist records 
+const item1 = new Item({
+  name: "Eat"
+})
+
+const item2 = new Item({
+  name: "Code"
+})
+
+const item3 = new Item({
+  name: "Sleep"
+})
+
+const DefaultArray = [item1, item2, item3];
+darry = ["Eat", "Code", "Sleep"]
+// ****************************************************************
+
+// Route list Schema
+const routeSchema = new mongoose.Schema({
+  name: String,
+  List: [todolist],
+})
+
+// Route list model
+const routeListModel = mongoose.model('routeList', routeSchema, 'routeList');
+
+// Route list record
+
+// function route() {
+//   const routeRecord = new routeListModel({
+//     name: string_recieved,
+//     list: [todolist]
+//   })
+// }
+
+routeList = [];
+
+
+
+// One time initialization
+Item.find().then(it => {
+  if (it.length == 0) {
+    Item.insertMany(DefaultArray).then(() => { console.log('One time initialisation Item inserted successfully') })
+  }
+});
+
+const workItems = [];
+
+app.get("/", function (req, res) {
+  dumm = [];
+  Item.find().then(it => {
+    for (i in it) {
+      dumm.push(it[i].name);
+    };
+    res.render("list", { listTitle: "Today", newListItems: dumm });
+  });
+});
+
+app.post("/", function (req, res) {
+  const item = req.body.newItem;
+  res.redirect("/");
+  const itemn = new Item({
+    name: item
+  })
+  itemn.save().then(() => { console.log('to do list inserted into collections successfully') });
+});
+
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.post("/delete", function (req, res) {
+  Item.deleteOne({ name: req.body.check }).then(() => {
+  });
+  res.redirect("/");
+})
+
+
+
+
+app.get("/:route", function (req, res) {
+  const directory = req.params.route;
+
+  routeListModel.findOne({ name: directory }).then(it => {
+
+    if (!it) {
+      res.render("list", { listTitle: directory, newListItems: darry });
+
+
+      const routeRecord = new routeListModel({
+        name: directory,
+        List: DefaultArray
+      })
+
+      routeRecord.save();
+
+
+
+    }
+
+    else {
+
+      ItemList = it.List;
+
+      passArray = [];
+      for (i in ItemList) {
+        passArray.push(ItemList[i].name);
+      }
+      res.render("list", { listTitle: directory, newListItems: passArray });
+    }
+
+  }
+  );
+
+
+})
+
+
+
+app.post("/:route", function (req, res) {
+
+  const directory = req.params.route;
+
+  var list = [];
+  routeListModel.findOne({ name: directory }).then(it => {
+
+
+    for (i in it.List) {
+      list.push( Item({
+        name: it.List[i].name
+      })
+      );
+
+    }
+
+    // routeListModel.updateOne({ name: directory }, { List: list }).then(newiwt => {
+    //   res.redirect("/" + directory);
+    // });
+
+
+    
+  list.push( Item({
+    name: req.body.newItem,
+  }))
+
+  
+  routeListModel.updateOne({ name: directory }, { List: list }).then(newiwt => {
+      res.redirect("/" + directory);
+    });
+
+
+  // res.redirect("/");
+
+
+  });
+
+  
+
+
+
+
+});
+
+
+app.listen(8080, function () {
+  console.log("Server started on port 8080");
+});
